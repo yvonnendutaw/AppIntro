a27fd6e1cc18c3671d4e3edbc589391935474bbd
yvonnendutaw <yvonita7@gmail.com>
2016-05-30 18:32:34 +0300

    added main activity

---
+++ app/src/main/java/com/example/yvonne/appintro/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.yvonne.appintro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+            }
