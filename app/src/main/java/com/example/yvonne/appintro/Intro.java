ef27f51fa4c0dd74c480a7a5987c549f3c2d052e
yvonnendutaw <yvonita7@gmail.com>
2016-05-30 17:17:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/yvonne/appintro/Intro.java
@@ -0,0 +1,62 @@
+package com.example.yvonne.appintro;
+
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import com.github.paolorotolo.appintro.AppIntro;
+
+
+
+public class Intro extends AppIntro {
+    // Please DO NOT override onCreate. Use init.
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        // Add your slide's fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        addSlide(SampleSlide.newInstance(R.layout.first_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.second_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.third_fragment));
+        addSlide(SampleSlide.newInstance(R.layout.fourth_fragment));
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+//        addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged() {
+        // Do something when the slide changes.
+    }
+
+    @Override
+    public void onNextPressed() {
+        // Do something when users tap on Next button.
+    }
+
+
+}
